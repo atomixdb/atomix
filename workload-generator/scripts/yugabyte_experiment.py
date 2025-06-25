cc52939571f9755e63f2c0f023869a7f7bcbc3d9
Yannis Zarkadas <yanniszark@gmail.com>
2025-06-25 00:25:07 -0600

    try to debug yugabyte pitr
    Signed-off-by: Yannis Zarkadas <yanniszark@gmail.com>

--- workload-generator/scripts/yugabyte_experiment.py
+++ workload-generator/scripts/yugabyte_experiment.py
@@ -21,9 +21,9 @@ def parse_args():
     parser = argparse.ArgumentParser()
     return parser.parse_args()
 
-def connect_to_yugabyte():
+def connect_to_yugabyte(host="localhost"):
     conn = psycopg2.connect(
-        host="localhost",
+        host=host,
         user="yugabyte",
         port=5433,
         password="yugabyte",
@@ -121,6 +121,42 @@ class YugabyteDB:
         safe_time_info = self.get_safe_time_info()
         print(safe_time_info)
 
+
+    def create_partition(self, from_container, to_container):
+        assert from_container in self.PRIMARY_CONTAINER or from_container in self.SECONDARY_CONTAINERS
+        assert to_container in self.PRIMARY_CONTAINER or to_container in self.SECONDARY_CONTAINERS
+        assert from_container != to_container
+
+        # Get from_container PID
+        from_container_pid = check_output(["docker", "inspect", "-f", "{{.State.Pid}}", from_container]).strip()
+
+        # Get to_container IP
+        to_container_ip = check_output(["docker", "inspect", "-f", "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}", to_container]).strip()
+
+        # Use nsenter to install an IPTables rule in the from_container's network namespace
+        # Block traffic from source to destination container using iptables
+        check_output([
+            "sudo", "nsenter", "--target", from_container_pid, "--net",
+            "iptables", "-A", "OUTPUT", "-d", to_container_ip, "-j", "DROP"
+        ])
+
+    def remove_partition(self, from_container, to_container):
+        assert from_container in self.PRIMARY_CONTAINER or from_container in self.SECONDARY_CONTAINERS
+        assert to_container in self.PRIMARY_CONTAINER or to_container in self.SECONDARY_CONTAINERS
+        assert from_container != to_container
+
+        # Get from_container PID
+        from_container_pid = check_output(["docker", "inspect", "-f", "{{.State.Pid}}", from_container]).strip()
+
+        # Get to_container IP
+        to_container_ip = check_output(["docker", "inspect", "-f", "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}", to_container]).strip()
+
+        # Use nsenter to remove the IPTables rule in the from_container's network namespace
+        check_output([
+            "sudo", "nsenter", "--target", from_container_pid, "--net",
+            "iptables", "-D", "OUTPUT", "-d", to_container_ip, "-j", "DROP"
+        ])
+
     def __start_docker_compose(self):
         if self.is_running():
             raise Exception("Docker compose is already running")
@@ -138,12 +174,15 @@ class YugabyteDB:
         secondary_address_1 = check_output(["docker", "inspect", "-f", "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}", self.SECONDARY_CONTAINERS[1]]).strip()
         if not primary_address or not secondary_address_0 or not secondary_address_1:
             raise Exception("Failed to get IP addresses for containers")
+        print(f"Primary: {primary_address}")
+        print(f"Secondary 1: {secondary_address_0}")
+        print(f"Secondary 2: {secondary_address_1}")
         self.PRIMARY_MASTER = primary_address
         self.SECONDARY_MASTER = secondary_address_0
         self.SECONDARY_MASTER_1 = secondary_address_1
         print("Docker compose started")
 
-    def failover_to_secondary(self):
+    def failover_to_secondary(self, safe_time=None):
         """Failover to the secondary cluster."""
         # Pause replication
         docker_check_output(
@@ -152,9 +191,10 @@ class YugabyteDB:
         )
 
         # Get safe time info
-        safe_time_info = self.get_safe_time_info()
-        safe_time = safe_time_info["safe_time_epoch"]
-        print(f"Replication info: {safe_time_info}")
+        if not safe_time:
+            safe_time_info = self.get_safe_time_info()
+            safe_time = safe_time_info["safe_time_epoch"]
+            print(f"Replication info: {safe_time_info}")
 
         # Get snapshot schedule ID
         schedules = docker_check_output(
@@ -169,7 +209,6 @@ class YugabyteDB:
 
         # Restore to safe time
         start_time = time.time()
-        safe_time = "minus 20s"
         docker_check_output(
             self.SECONDARY_CONTAINERS[0],
             f"./bin/yb-admin --master_addresses {self.SECONDARY_MASTER} restore_snapshot_schedule {schedule_id} {safe_time}"
@@ -271,7 +310,7 @@ def insert_row(cursor, key, value):
     """
     cursor.execute(insert_sql, (key, value))
 
-def insert_rows(cursor, key_prefix, value_size, num_rows):
+def insert_rows(cursor, key_prefix, value, num_rows):
     """Insert multiple rows into the kv_store table.
 
     Args:
@@ -280,7 +319,7 @@ def insert_rows(cursor, key_prefix, value_size, num_rows):
         value_size (int): Size in bytes of the value to insert
         num_rows (int): Number of rows to insert
     """
-    value = random.randbytes(value_size).hex()
+    # value = random.randbytes(value_size).hex()
 
     for i in range(num_rows):
         key = f"{key_prefix}-{i}"
@@ -301,23 +340,120 @@ def main():
     # Insert a bunch of rows
     print("Inserting rows")
     start_time = time.time()
-    cursor = connect_to_yugabyte()
+    cursor = connect_to_yugabyte(yb.PRIMARY_MASTER)
+    secondary_cursor = connect_to_yugabyte(yb.SECONDARY_MASTER)
     num_iter = 10000
-    num_rows_per_partition = 10
-    for _ in range(num_iter):
-        uuid_prefix = str(uuid.uuid4())
-        insert_rows(cursor, "a" + uuid_prefix, 100, num_rows_per_partition)
-        insert_rows(cursor, "e" + uuid_prefix, 100, num_rows_per_partition)
-        insert_rows(cursor, "n" + uuid_prefix, 100, num_rows_per_partition)
-        insert_rows(cursor, "z" + uuid_prefix, 100, num_rows_per_partition)
+    num_rows_per_partition = 1
+    for i in range(num_iter):
+        uuid_prefix = str(i)
+        # uuid_prefix = str(uuid.uuid4())
+        insert_rows(cursor, "a" + uuid_prefix, "1", num_rows_per_partition)
+        insert_rows(cursor, "e" + uuid_prefix, "1", num_rows_per_partition)
+        insert_rows(cursor, "n" + uuid_prefix, "1", num_rows_per_partition)
+        insert_rows(cursor, "z" + uuid_prefix, "1", num_rows_per_partition)
     duration = time.time() - start_time
     num_rows = num_iter * num_rows_per_partition * 4
     print(f"Inserted {num_rows} rows in {duration:.2f} seconds")
 
+    # Ensure all rows are visible on the secondary
+    time.sleep(5)
+    print("Performing full table scan to ensure replication")
+    scan_sql = "SELECT * FROM kv_store"
+    secondary_cursor.execute(scan_sql)
+    count = 0
+    page_size = 1000
+    while True:
+        rows = secondary_cursor.fetchmany(page_size)
+        if not rows:
+            break
+        for row in rows:
+            if row[1] != "1":
+                raise Exception(f"Unexpected value {row[1]} found in row {row[0]}, expected '1'")
+            count += 1
+    print(f"Verified {count} rows have value '1'")
+    expected_count = num_iter * num_rows_per_partition * 4
+    assert count == expected_count, f"Expected {expected_count} rows, got {count}"
+    safe_time_info = yb.get_safe_time_info()
+    print(f"Before partition: Safe time info: {safe_time_info}")
+    safe_time_before_partition = safe_time_info["safe_time_epoch"]
+
+    # Create partition
+    print("Creating partition")
+    yb.create_partition(yb.PRIMARY_CONTAINER, yb.SECONDARY_CONTAINERS[0])
+    time.sleep(5)
+
+    # Insert rows
+    print("Inserting rows")
+    num_iter_after = num_iter * 1
+    for i in range(num_iter_after):
+        uuid_prefix = str(i) + "_"
+        insert_rows(cursor, "a" + uuid_prefix, "2", num_rows_per_partition)
+        insert_rows(cursor, "e" + uuid_prefix, "2", num_rows_per_partition)
+        insert_rows(cursor, "n" + uuid_prefix, "2", num_rows_per_partition)
+        insert_rows(cursor, "z" + uuid_prefix, "2", num_rows_per_partition)
+
+    # Removeme
+    time.sleep(10**9)
+
+    # Ensure new rows are not visible on the secondary
+    time.sleep(1)
+    print("After partition: Performing full table scan to ensure transactional consistency")
+    scan_sql = "SELECT * FROM kv_store"
+    secondary_cursor.execute(scan_sql)
+    count = 0
+    page_size = 1000
+    while True:
+        rows = secondary_cursor.fetchmany(page_size)
+        if not rows:
+            break
+        for row in rows:
+            if row[1] != "1":
+                raise Exception(f"After partition: Unexpected value {row[1]} found in row {row[0]}, expected '1'")
+            count += 1
+    print(f"After partition: Verified {count} rows have value '1'")
+    expected_count = num_iter * num_rows_per_partition * 4
+    assert count == expected_count, f"After partition: Expected {expected_count} rows, got {count}"
+
+    safe_time_info = yb.get_safe_time_info()
+    print(f"After partition: Safe time info: {safe_time_info}")
+
     # Perform failover
     print("Performing failover")
-    yb.failover_to_secondary()
+    start_time = time.time()
+    yb.failover_to_secondary(safe_time=safe_time_before_partition)
+    duration = time.time() - start_time
+    print(f"Failover completed in {duration:.2f} seconds")
 
+    # Insert a single row
+    print("Inserting a single row")
+    insert_row(secondary_cursor, "a", "1")
+    insert_row(secondary_cursor, "e", "1")
+    insert_row(secondary_cursor, "n", "1")
+    insert_row(secondary_cursor, "z", "1")
+
+    time.sleep(1)
+    print("After failover: Performing full table scan to ensure transactional consistency")
+    scan_sql = "SELECT * FROM kv_store"
+    secondary_cursor.execute(scan_sql)
+    count = 0
+    page_size = 1000
+    while True:
+        rows = secondary_cursor.fetchmany(page_size)
+        if not rows:
+            break
+        for row in rows:
+            if row[1] != "1":
+                raise Exception(f"After failover: Unexpected value {row[1]} found in row {row[0]}, expected '1'")
+            count += 1
+    print(f"After failover: Verified {count} rows have value '1'")
+    expected_count = num_iter * num_rows_per_partition * 4 + 4
+    assert count == expected_count, f"After partition: Expected {expected_count} rows, got {count}"
+
+    print("Waiting a bit before teardown")
+    time.sleep(1000)
+
+    # Teardown
+    print("Teardown")
     yb.teardown()
 
 
